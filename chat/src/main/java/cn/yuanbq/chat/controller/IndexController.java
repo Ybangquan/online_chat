1d859bc0654f221119cf98abd4668884d40068f2
Ybangquan <2848339859@qq.com>
2018-06-11 16:29:25 +0800

    chat

---
+++ chat/src/main/java/cn/yuanbq/chat/controller/IndexController.java
@@ -0,0 +1,39 @@
+package cn.yuanbq.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "/html/index.html";
+    }
+
+    /**
+     * 获取当前网络ip
+     * @param request
+     * @return
+     */
+    @RequestMapping("/getIpAddr")
+    @ResponseBody
+    public String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
